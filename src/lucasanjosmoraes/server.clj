ae10cfe65966b1e28b34cc20061e017de6969c68
Lucas Moraes <lucas.anjos.moraes@gmail.com>
2021-12-05 18:58:34 -0400

    feat(main): create helpers to test project in the REPL

--- src/lucasanjosmoraes/server.clj
+++ src/lucasanjosmoraes/server.clj
@@ -2,11 +2,19 @@
   (:gen-class)
   (:require [io.pedestal.http :as server]
             [io.pedestal.http.route :as route]
+            [io.pedestal.test :as test]
             [lucasanjosmoraes.service :as service]))
+
+(defonce custom-server (atom nil))
 
 ;; This is an adapted service map, that can be started and stopped
 ;; From the REPL you can call server/start and server/stop on this service
 (defonce runnable-service (server/create-server service/service))
+
+;; Helpers
+
+(defn test-request [verb url]
+  (test/response-for (::server/service-fn @custom-server) verb url))
 
 (defn run-dev
   "The entry-point for 'lein run-dev'"
@@ -27,6 +35,17 @@
       server/dev-interceptors
       server/create-server
       server/start))
+
+(defn start-dev []
+  (reset! custom-server
+          (run-dev)))
+
+(defn stop-dev []
+  (server/stop @custom-server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
 
 (defn -main
   "The entry-point for 'lein run'"
