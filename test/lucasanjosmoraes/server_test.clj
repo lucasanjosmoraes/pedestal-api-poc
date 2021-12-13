028743f4884f86bd8fd743aa1269ccfeba89aee8
Lucas Moraes <lucas.anjos.moraes@gmail.com>
2021-12-12 20:35:00 -0400

    chore: add default test file

---
+++ test/lucasanjosmoraes/server_test.clj
@@ -0,0 +1,7 @@
+(ns lucasanjosmoraes.server-test
+  (:require [clojure.test :refer :all]
+            [lucasanjosmoraes.server :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
