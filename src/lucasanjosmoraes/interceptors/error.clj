98122ab21e73cf97d5eba4df7acebdd7a66731d5
Lucas Moraes <lucas.anjos.moraes@gmail.com>
2022-01-22 15:56:47 -0400

    chore(lucasanjosmoraes.i.error): add comment to the handler

--- src/lucasanjosmoraes/interceptors/error.clj
+++ src/lucasanjosmoraes/interceptors/error.clj
@@ -3,6 +3,8 @@
 
 (def service-error-handler
   (error-int/error-dispatch [ctx ex]
+    ;; Any validation error from the spec is of the type java.lang.AssertionError.
+    ;; In a production-ready application, we must handle its exceptions properly.
     [{:exception-type :java.lang.AssertionError}]
     (assoc ctx :response {:status 400 :body "Invalid request"})
 
