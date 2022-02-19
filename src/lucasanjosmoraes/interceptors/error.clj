bcaebe4fcb857de468d7c0609b5d9d374f90f971
Lucas Moraes <lucas.anjos.moraes@gmail.com>
2022-02-19 18:40:39 -0400

    chore(src/i.error): add comment to the service-error-handler

--- src/lucasanjosmoraes/interceptors/error.clj
+++ src/lucasanjosmoraes/interceptors/error.clj
@@ -4,6 +4,8 @@
 
 (def service-error-handler
   (error-int/error-dispatch [ctx ex]
+    ;; Any validation error from the schema is of the type clojure.lang.ExceptionInfo.
+    ;; In a production-ready application, we must handle its exceptions properly.
     [{:exception-type ExceptionInfo}]
     (assoc ctx :response {:status 500
                           :body   "Internal Error"})
